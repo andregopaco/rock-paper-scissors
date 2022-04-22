3079902e62f65d316a89bafe7fab8e20c891fcb2
André Gomes Pacheco <andregp86@gmail.com>
2022-04-21 22:34:07 -0300

    Write pseudocode and array of choices

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+// console.log("Hello world!");
+
+// Create an array with the three possible answers;
+const choices = ['Rock', 'Paper', 'Scissors']
+
+// Make the computer randomly pick one of those answers;
+function computerPlay() {
+  
+}
+
+// The player shall input his/her option;
+
+
+// Both answers are compared;
+
+// If the answers are the same, it's a tie;
+
+// Else (answers are different)
+// If player choice is rock it wins if computer picks scissors; otherwise player loses;
+// If player choice is paper it wins if computer picks rock; otherwise player lose;
+// If player choice is scissors it wins if computer picks paper; otherwise player lose;
