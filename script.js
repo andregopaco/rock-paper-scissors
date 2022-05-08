b36b0b716a5427e05a867db17577b3d005b5d1ce
André Gomes Pacheco <andregp86@gmail.com>
2022-05-07 22:44:15 -0300

    Show score on screen

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-// console.log("Hello world!");
-
-// Create an array with the three possible answers;
 const choicesList = ['rock', 'paper', 'scissors'];
 
-// Make the computer randomly pick one of those answers;
 function computerPlay() {
   let randomNumber = Math.floor(Math.random()*choicesList.length);
   let computerChoice = choicesList[randomNumber];
   return computerChoice;
 }
-
-// The player shall input his/her option;
-function playerPlay() {
-  let choiceNotListed = true;
-  while (choiceNotListed) {
-    playerChoice = prompt("Choose one: rock, paper or scissors?").toLowerCase();
-    if (choicesList.includes(playerChoice)) {
-      choiceNotListed = false;
-      return playerChoice;
-    } else {
-    alert("Answer invalid");
-    }
-  }   
-}
-
-// Both answers are compared; If the answers are the same, it's a tie;
-// Else (answers are different): 
-// If player choice is rock it wins if computer picks scissors; otherwise player loses;
-// If player choice is paper it wins if computer picks rock; otherwise player lose;
-// If player choice is scissors it wins if computer picks paper; otherwise player lose;
 
 function playRound (playerSelection, computerSelection) {  
   if (playerSelection === computerSelection) {
@@ -50,49 +26,35 @@ function playRound (playerSelection, computerSelection) {
   }
 }
 
-
-// function game() {
-//   let keepGoing = true;
-//   let playerScore = 0;
-//   let computerScore = 0;
-
-//   while (keepGoing) {
-
-//     let playerSelection = playerPlay();
-//     console.log(playerSelection);
-//     let computerSelection = computerPlay();
-//     console.log(computerSelection);
-
-//     let roundResult = playRound(playerSelection, computerSelection);
-//     console.log(roundResult);
-    
-//     if (roundResult.includes('win')) {
-//       playerScore++;
-//     } else if (roundResult.includes('lose')) {
-//       computerScore += 1;
-//     }
-
-//     if (computerScore === 5 || playerScore === 5) {
-//       console.log(`You've reached the end of this game. The final score is: \nPlayer: ${playerScore} x Computer: ${computerScore}.`)
-//       keepGoing = false;
-//     } else {
-//       console.log(`Player: ${playerScore} x Computer: ${computerScore}.`)
-//     }
-//   }
-// }
-
-const resultDiv = document.querySelector('.gameResult');
+let playerScore = 0;
+let computerScore = 0;
+const div = document.querySelector('.resultDiv');
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    let playerSelection = button.id;
     let computerSelection = computerPlay();
+    let playerSelection = button.id;
     let roundResult = playRound(playerSelection, computerSelection);
-    resultDiv.textContent = `You choose ${playerSelection}. The computer chooses ${computerSelection}.
-    ${roundResult}`;
+
+    if (roundResult.includes('win')) {
+      playerScore++;
+    } else if (roundResult.includes('lose')) {
+      computerScore += 1;
+    }
+
+    div.textContent = `You choose ${playerSelection}. The computer chooses ${computerSelection}. ${roundResult}. Current score: player: - ${playerScore} x computer: ${computerScore}`;
+
+      // TODO: append score on page;
+    if (playerScore === 5) {
+      div.textContent += ` You win the game!`;
+    } else if (computerScore === 5) {
+      div.textContent = `You lost the game.`;
+    }
   });
 });
+
+
 
 
 
